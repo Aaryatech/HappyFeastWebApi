b71fe4777c66e683daeca6ddce52a77c50398941
Viplav Thoke <viplavthoke@gmail.com>
2018-07-31 15:52:56 +0530

    /akshay

--- src/main/java/com/ats/feastwebapi/repository/GetBillRepository.java
+++ src/main/java/com/ats/feastwebapi/repository/GetBillRepository.java
@@ -10,7 +10,7 @@ import com.ats.feastwebapi.model.GetBill;
 
 public interface GetBillRepository extends JpaRepository<GetBill, Integer>{
 
-	@Query(value = "select * from t_bill where bill_date =:date and enter_by =:type", nativeQuery = true)  
+	@Query(value = "select * from t_bill where bill_date =:date and enter_by =:type order by bill_id desc", nativeQuery = true)  
 	List<GetBill> getBillByDate(@Param("date")String date, @Param("type")int type);
 	
 	@Query(value = "select po.name from t_bill_details bd,t_parcel_order po where po.parcel_order_id = bd.order_id and bd.order_id=:orderId group by name", nativeQuery = true)  
